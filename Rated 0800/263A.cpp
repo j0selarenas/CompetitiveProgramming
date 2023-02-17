1b701738c4e82fddcd85c18ae832ce1164e2c8c2
Jose Larenas <111939223+j0selarenas@users.noreply.github.com>
2023-02-17 14:21:39 -0300

    CodeForces - Helpful Maths

--- Rated 0800/263A.cpp
+++ Rated 0800/263A.cpp
@@ -1,26 +1,32 @@
-#include <iostream>
-#include <cmath>
+#include <bits/stdc++.h> 
 using namespace std ;
 
 // Beautiful Matrix
 int main()
 {
-	int matrix[5][5] ;
-	int x, y ;
-	for (int i = 0; i < 5; ++i)
+	string input ;
+	cin >> input ;
+	int size = input.length()-count(input.begin(), input.end(), '+') ;
+	int array[size] ;
+	int idx = 0;
+
+	for (int i = 0; i < input.length(); i++)
 	{
-		for (int j = 0; j < 5; ++j)
-		{
-			cin >> matrix[i][j] ;
-			if (matrix[i][j] == 1)
-			{
-				x = j ;
-				y = i ;
-			}
-		}
+		if (input[i] == '+')
+			idx ++ ;
+		else
+			array[i - idx] = int(input[i]-'0') ;
 	}
 
-	cout << abs(2-x)+abs(2-y) ;
+	sort((array), array + size) ;
+
+	for (int i = 0; i < size; i++)
+	{
+		if (i != size-1)
+			cout << array[i] << "+" ;
+		else
+			cout << array[i] ;
+	}
 
 	return 0 ;
 }
