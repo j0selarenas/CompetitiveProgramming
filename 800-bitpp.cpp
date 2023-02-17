86724d695f27e45abe4a8bfd871cae035bf2c899
Jose Larenas <111939223+j0selarenas@users.noreply.github.com>
2023-02-17 13:28:56 -0300

    Bit++ CodeForces

---
+++ 800-bitpp.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std ;
+ 
+// Bit ++ (Rated 800)
+int main()
+{
+	int n ;
+	int output = 0 ;
+	cin >> n ;
+	for (int i = 0; i < n; i++)
+	{
+		string input ;
+		cin >> input ;
+		if (input.find("+") != string::npos)
+			output ++ ;
+		else
+			output -- ;
+	}
+	cout << output ;
+	return 0 ;
+}
