d6bd3314b0889a0d931612ba4dcf5560e091522a
Jose Larenas <111939223+j0selarenas@users.noreply.github.com>
2023-02-21 14:14:26 -0300

    CodeForces - cAPS lOCK

--- Rated 1000/131A.cpp
+++ Rated 1000/131A.cpp
@@ -2,7 +2,30 @@
 using namespace std;
 
 void solver(){
-	
+	string s ;
+	cin >> s ;
+
+	string fixed = "" ;
+
+	int length = s.length() ;
+	int count = 0 ;
+
+	for (int i = 0; i < length; ++i)
+	{
+		if (i > 0){
+			if (isupper(s[i]))
+				count ++ ;
+		}
+		if (isupper(s[i]))
+			fixed += tolower(s[i]) ;
+		if (islower(s[i]))
+			fixed += toupper(s[i]) ;
+	}
+
+	if (count == length - 1)
+		cout << fixed ;
+	else
+		cout << s ;
 }
 
 int main(){
